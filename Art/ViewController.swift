c01753d3db6c0f3701d3ec0ded03ee0d1378334b
Diya Jolie Mohan <diyajolie@Diyas-MacBook-Air.local>
2017-07-21 15:07:49 -0400

    Second draft drawpixel

--- Art/ViewController.swift
+++ Art/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SceneKit
 
 class ViewController: UIViewController {
     
@@ -15,12 +16,14 @@ class ViewController: UIViewController {
     var wCount: Int = 0
     var currentColor: UIColor = .black
     var buttonArr: [UIButton] = []
-    
+    var buttonArr2: [UIButton] = []
+//    var buttonCenter: [UIButton: CGPoint] = [:]
     
     let screenSize = UIScreen.main.bounds
-    let picView = UIView(frame: CGRect(x: 7, y: 130, width: 400, height: 500))
-    let toolView = UIView(frame: CGRect(x: 7, y: 627, width: 400, height: 100))
+    let picView = UIView(frame: CGRect(x: 7, y: 130, width: 400, height: 490))
+    let toolView = UIView(frame: CGRect(x: 7, y: 630, width: 400, height: 100))
     let toolView2 = UIView(frame: CGRect(x: 7, y: 20, width: 400, height: 100))
+    let drawView = UIView(frame: CGRect(x: 15, y: 15, width: 25, height: 25))
     
     let buttonRed = UIButton(frame: CGRect(x: 25, y: 25, width: 50, height: 50))
     let buttonBlue = UIButton(frame: CGRect(x: 85, y: 25, width: 50, height: 50))
@@ -34,6 +37,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.addSubview(picView)
         view.addSubview(toolView)
         view.addSubview(toolView2)
@@ -76,7 +80,16 @@ class ViewController: UIViewController {
         picView.backgroundColor = UIColor.white
         toolView.backgroundColor = UIColor.white
         toolView2.backgroundColor = UIColor.white
+        drawView.backgroundColor = UIColor.black
+        
+        picView.addSubview(drawView)
+        drawView.layer.cornerRadius = 12
+        drawView.layer.masksToBounds = true
+        let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        self.drawView.addGestureRecognizer(gestureRecognizer)
+        
     }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -88,8 +101,16 @@ class ViewController: UIViewController {
                 let button = UIButton(frame: CGRect(x: wCount, y: hCount, width: length, height: length))
                 button.addTarget(self, action: #selector(buttonP), for: .touchDown)
                 
+                
                 picView.addSubview(button)
                 wCount +=  length
+                
+                buttonArr2.append(button)
+                
+//                for button in buttonArr2 {
+//                    buttonCenter[button] = button.center
+//                }
+                
             }
             wCount = 0
             hCount += length
@@ -126,8 +147,46 @@ class ViewController: UIViewController {
     }
     
     func buttonUndoP(sender: UIButton) {
-        buttonArr.last!.backgroundColor = UIColor.white
-        buttonArr.popLast()
+        if buttonArr != [] {
+            buttonArr.last!.backgroundColor = UIColor.white
+            buttonArr.popLast()
+        }
+    }
+    
+    func handlePan(_ gestureRecognizer: UIPanGestureRecognizer) {
+        if gestureRecognizer.state == .began || gestureRecognizer.state == .changed {
+            
+            let translation = gestureRecognizer.translation(in: self.view)
+            // note: 'view' is optional and need to be unwrapped
+            gestureRecognizer.view!.center = CGPoint(x: gestureRecognizer.view!.center.x + translation.x, y: gestureRecognizer.view!.center.y + translation.y)
+            gestureRecognizer.setTranslation(CGPoint.zero, in: self.view)
+            
+            
+//            for (button, center) in buttonCenter {
+//                print("button center: \(center), drawView center: \(drawView.center)")
+//                if center == drawView.center {
+//                    
+//                    buttonP(button: button)
+//                    print("it works")
+//                }
+//            }
+            
+        }
+        
+       // func sizer(button: UIButton) {
+            
+            for button in buttonArr2 {
+                let rangeX = (((button.frame.size.width / 2) - button.center.x)...((button.frame.size.width / 2) + button.center.x))
+                let rangeY = (((button.frame.size.height / 2) - button.center.y)...((button.frame.size.height / 2) + button.center.y))
+                
+                if rangeX.contains(drawView.center.x) && rangeY.contains(drawView.center.y) {
+                    self.buttonP(button: button)
+                    print("it works")
+                }
+            }
+            
+    //}
+    
     }
 }
 
